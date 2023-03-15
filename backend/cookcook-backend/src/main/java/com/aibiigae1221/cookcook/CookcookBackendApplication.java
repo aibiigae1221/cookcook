c16e91f14d43e2e4741643e01d9e4c9a8a095e22
kkh <aibiigae1221@gamil.com>
2023-03-15 18:33:29 +0900

    백엔드 프로젝트 생성

---
+++ backend/cookcook-backend/src/main/java/com/aibiigae1221/cookcook/CookcookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aibiigae1221.cookcook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookcookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookcookBackendApplication.class, args);
+	}
+
+}
---
+++ backend/cookcook-backend/src/test/java/com/aibiigae1221/cookcook/CookcookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aibiigae1221.cookcook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookcookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
