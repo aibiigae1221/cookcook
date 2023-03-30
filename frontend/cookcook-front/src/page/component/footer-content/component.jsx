e0819953c3dc3a6326db2dc710a51c672c2d53e0
kkh <aibiigae1221@gamil.com>
2023-03-30 20:38:22 +0900

    주소 부분 address 태그 부여

--- frontend/cookcook-front/src/page/component/footer-content/component.jsx
+++ frontend/cookcook-front/src/page/component/footer-content/component.jsx
@@ -5,16 +5,18 @@ const FooterContent = () => {
   return (
     <>
       <div className={styles.wrapper}>
-        <ul className={styles.list}>
-          <li className={styles.listItem}>
-            <h1 className={styles.header}>I am</h1>
-          </li>
-          <li className={styles.listItem}>웹 개발자가 되고 싶은 김경훈입니다.</li>
-          <li className={styles.listItem}>백엔드와 프론트엔드를 전부 다루는 풀스택 개발자가 되고 싶습니다.</li>
-          <li className={styles.listItem}>TEL: 010-9458-8336</li>
-          <li className={styles.listItem}>EMAIL: aibiigae1221@gmail.com</li>
+        <address>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <h1 className={styles.header}>I am</h1>
+            </li>
+            <li className={styles.listItem}>웹 개발자가 되고 싶은 김경훈입니다.</li>
+            <li className={styles.listItem}>백엔드와 프론트엔드를 전부 다루는 풀스택 개발자가 되고 싶습니다.</li>
+            <li className={styles.listItem}>TEL: 010-9458-8336</li>
+            <li className={styles.listItem}>EMAIL: aibiigae1221@gmail.com</li>
 
-        </ul>
+          </ul>
+        </address>
 
         <div className={styles.verticalDivider}></div>
 
