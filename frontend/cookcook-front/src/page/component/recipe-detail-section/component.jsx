d28ba28d18b7677ecef64d2df194017f6b384118
kkh <aibiigae1221@gamil.com>
2023-04-08 19:21:19 +0900

    레시피 디테일 페이지 디자인 리워크

--- frontend/cookcook-front/src/page/component/recipe-detail-section/component.jsx
+++ frontend/cookcook-front/src/page/component/recipe-detail-section/component.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import defaultCookImage from "./default-cook-image.jpg";
-import userIcon from "./user.svg";
-import calendarIcon from "./calendar.svg";
 import dompurify from "dompurify";
 
 
@@ -12,82 +10,70 @@ const RecipeDetailSection = ({recipe}) => {
   if(!recipe)
     return <></>;
 
-  const sample = "<span style='color:red' data-text='a1'>asd</span>";
-
   const sanitize = dompurify.sanitize;
   const sanitizeOption = {
-    ALLOWED_TAGS: ['span', 'p', 'h1', 'h2', 'h3', 'h4', 'a'],
+    ALLOWED_TAGS: ['span', 'p', 'h1', 'h2', 'h3', 'h4', 'a', 'br'],
     ALLOWED_ATTR: ['style', 'href'],
     ALLOW_DATA_ATTR: false
   };
 
-
+  const moveToTop = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  };
 
   return (
     <div className={styles.wrap}>
 
       <div className={styles.basicInfo}>
-          <h1 className={styles.h1}>{recipe.title}</h1>
+          <h1 className={`${styles.h1} ${styles.title}`}>{recipe.title}</h1>
 
-          {recipe.mainImageUrl ?
+          {(recipe.mainImageUrl !== null && recipe.mainImageUrl !== "")?
+
             <img src={recipe.mainImageUrl} alt={recipe.mainImageUrl} className={styles.mainImage} />
             :
             <img src={defaultCookImage} alt={defaultCookImage} className={styles.mainImage} />
           }
 
-          <div className={styles.detailContainer}>
-            <h2 className={styles.h2}>DETAIL</h2>
-            <ul>
-              <li>
-                <img src={userIcon} alt={userIcon} className={styles.icon} />
-                <span>{recipe.user.nickname}</span>
-              </li>
-              <li>
-                <img src={calendarIcon} alt={calendarIcon} className={styles.icon} />
-                {recipe.createdDateFormatted}
-              </li>
-              <li>
-
-                {recipe.tags.length > 0 && recipe.tags.map(tag =>
-                  <span key={tag.tagId} className={styles.tag}>#{tag.tagName}</span>
-                )}
-              </li>
-            </ul>
-
-
-            <p className={styles.description} dangerouslySetInnerHTML={{__html:sanitize(recipe.commentary, sanitizeOption)}} />
+          <div className={styles.description}>
+            <p className={styles.inline}>
+              <span className={`${styles.userIcon} ${styles.icon}`}></span>
+              <span>{recipe.user.nickname}</span>
+              <span className={`${styles.dateIcon} ${styles.icon}`}></span>
+              <span>{recipe.createdDateFormatted}</span>
+            </p>
+    
+            <p>
+              <span className={`${styles.tagIcon} ${styles.icon}`}></span>
+              {recipe.tags.length > 0 && recipe.tags.map(tag => 
+                <span key={tag.tagId} className={styles.tag}>{tag.tagName}</span>  
+              )}
+            </p>
+            <p className={styles.commentary} dangerouslySetInnerHTML={{__html:sanitize(recipe.commentary, sanitizeOption)}} />
           </div>
-
-
       </div>
 
-      <div>
-        <h2 className={styles.h1}>조리 과정</h2>
-
-        <ul className={styles.stepList}>
-
-        {recipe.stepList.length > 0 && recipe.stepList.map(step =>
-          <React.Fragment key={step.stepId}>
-            <li className={styles.stepImage}>
-              {(step.imageUrl && step.imageUrl !== "")?
-                <img src={step.imageUrl} alt={step.imageUrl} className={styles.stepImage} />
-              :
-                <img src={defaultCookImage} alt={defaultCookImage} className={styles.stepImage} />
+      <div className={styles.stepListContainer}>
+        <h1 className={styles.h1}>조리 과정</h1>
+        
+        <ul className={styles.list}>
+          {recipe.stepList.length > 0 && recipe.stepList.map((step, idx) => 
+            <li key={step.stepId}>
+              {(step.imageUrl !== null && step.imageUrl !== "")?
+                <img src={step.imageUrl} alt={step.imageUrl} />
+                :
+                <img src={defaultCookImage} alt={step.defaultCookImage} />
               }
-
-
+              <div className={styles.detail}>
+                <strong className={styles.order}>#{(idx+1)}</strong>
+                <p dangerouslySetInnerHTML={{__html:sanitize(step.detail, sanitizeOption)}} />
+              </div>
             </li>
-            <li className={styles.detail}>
-              <strong className={styles.stepDetailStrong}>#{Number(step.stepNumber)+1}</strong>
-              <p dangerouslySetInnerHTML={{__html:sanitize(step.detail, sanitizeOption)}} />
-            </li>
-          </React.Fragment>
-        )}
+          )}
+        </ul>      
+      </div>
 
-        </ul>
-
-
-
+      <div className={styles.navButtons}>
+          <button onClick={moveToTop}>위로 가기</button>
       </div>
 
     </div>
